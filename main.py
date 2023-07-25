71433dc746064be6f3bd0a72bc1c2410eb77d44a
ribeirolucas962 <lucasribeiro962@gmail.com>
2023-07-24 21:25:34 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#declarando as listas cazias
+cardapio = []
+comida = []
+
+#pedindo para o usuário fornecer os valores
+
+for c in range(0,4):
+    comida.append ( str(input('Fale o nome do lanche:')))
+    comida.append ( float(input('Fale o valor da comida:')))
+    cardapio.append (comida[:])
+    comida.clear()
+
+print(cardapio)
+
+
+
